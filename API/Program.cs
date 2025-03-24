c29255ade315606d57824258a9aa9a7e313b9f1c
salemsaada <89607759+salemsaada@users.noreply.github.com>
2025-03-24 05:02:27 -0600

    end of section 4

--- API/Program.cs
+++ API/Program.cs
@@ -1,3 +1,5 @@
+using Application.Activities.Queries;
+using Application.Core;
 using Microsoft.EntityFrameworkCore;
 using Persistence;
 var builder = WebApplication.CreateBuilder(args);
@@ -5,19 +7,20 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
-builder.Services.AddDbContext<AppDbContext>(opt => 
+builder.Services.AddDbContext<AppDbContext>(opt =>
 {
     opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
 builder.Services.AddCors();
-
+builder.Services.AddMediatR(x => x.RegisterServicesFromAssemblyContaining<GetActivityList.Handler>());
+builder.Services.AddAutoMapper(typeof(MappingProfiles).Assembly);
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-app.UseCors(x => x.WithOrigins("https://localhost:3000","http://localhost:3000").AllowAnyHeader().AllowAnyMethod());
+app.UseCors(x => x.WithOrigins("https://localhost:3000", "http://localhost:3000").AllowAnyHeader().AllowAnyMethod());
 app.MapControllers();
-using var scope  = app.Services.CreateScope();
-var services =  scope.ServiceProvider;
+using var scope = app.Services.CreateScope();
+var services = scope.ServiceProvider;
 try
 {
     var context = services.GetRequiredService<AppDbContext>();
@@ -28,6 +31,6 @@ catch (Exception ex)
 {
     var logger = services.GetRequiredService<Logger<Program>>();
     logger.LogError(ex, "an error accured during migration");
-    
+
 }
 app.Run();
